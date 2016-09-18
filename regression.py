28185cd1e84fb9f88960daecc282badb583e83b7
vidit23 <viditvineet@hotmail.com>
2016-09-18 05:55:55 +0530

    Added visualisation

--- regression.py
+++ regression.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import Quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('ggplot')
 df = Quandl.get('WIKI/GOOGL')
 
 # Selecting the columns we need
@@ -28,22 +31,42 @@ print(forecast_out)
 
 # Label for each day becomes some "Adj. Close" of the future
 df['label'] = df[forecast_col].shift(-1*forecast_out)
-df.dropna(inplace = True)
 
 X = np.array(df.drop(['label'], 1))
-Y = np.array(df['label'])
-
 # To make all the feature value between 0 and 1
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace = True)
+Y = np.array(df['label'])
+
 
 X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.2)
 
-clf = LinearRegression()
+# n_jobs signifies how many threads we are willing to run
+clf = LinearRegression(n_jobs = 10)
+
 # To use Support Vector Machine
-# clf = svm.SVR()
-
+# clf = svm.SVR(kernel='poly')
 clf.fit(X_train, Y_train)
-
 accuracy = clf.score(X_test, Y_test)
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
 
-print(accuracy)
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc = 4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
